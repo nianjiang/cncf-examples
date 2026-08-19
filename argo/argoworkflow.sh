84a7d886b6de1760a7fc9d18979fd1218804225d
Nian Jiang <nothing123@126.com>
2026-08-19 10:04:23 +0800

    argo install

---
+++ argo/argoworkflow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+install() {
+    kubectl create ns argo
+    kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/v3.7.6/install.yaml
+}
+
+
+# 定义主函数
+main() {
+   echo "Start..............."
+
+
+    install
+
+
+
+
+
+    echo "End..............."
+}
+
+# 脚本入口：执行 main 函数并传递所有参数
+main "$@"
